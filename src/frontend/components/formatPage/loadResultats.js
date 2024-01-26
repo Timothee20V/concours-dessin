cc0f70c0844ce9579271254b345fed38c0ce6b0a
Mathis DIBKE <mathis.dibke@reseau.eseo.fr>
2024-01-26 15:21:45 +0100

    dev result concours + Fix compétiteur

--- src/frontend/components/formatPage/loadResultats.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    $("#results-table").load("../formatPage/resultatsConcours/results-table.html");
-});
--- src/frontend/components/formatPage/navbar/deconnexion.js
+++
@@ -1,7 +0,0 @@
-const deconnexion = () => {
-    console.log("deconnexion");
-    //localStorage.removeItem("token");
-    window.location = "./index.html";
-}
-
-document.getElementById("deconnexion").addEventListener("click", deconnexion);
